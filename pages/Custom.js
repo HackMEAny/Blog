97f81ba6c09a78de2d1a70ca1f371c5255232a6c
HackMEAny <hackme.any@protonmail.com>
2022-06-27 18:39:21 +0530

    MDX custom file added

---
+++ pages/Custom.js
@@ -0,0 +1,18 @@
+import { Divider, Typography } from "@mui/material";
+
+const Hr = () => {
+  return <Divider variant="middle" />;
+};
+const custom = {
+  h1: (props) => <Typography {...props} color="primary.main" variant="h1" />,
+  h2: (props) => <Typography {...props} variant="h2" />,
+  h3: (props) => <Typography {...props} variant="h3" />,
+  h4: (props) => <Typography {...props} variant="h4" />,
+  h5: (props) => <Typography {...props} variant="h5" />,
+  h6: (props) => <Typography {...props} variant="h6" />,
+  p: (props) => <Typography {...props} variant="body1" />,
+  p2: (props) => <Typography {...props} variant="body2" />,
+  hr: (props) => <Divider {...props} varient="full" />,
+  hr1: (props) => Hr,
+};
+export default custom;
